92f042f71abc1dd818711d25fcb385f662bff10e
jpatrick5402 <jpatrick5402@gmail.com>
2025-08-03 14:31:56 -0400

    Adding basic hierarchy, need to add button functionality

--- app/pages/data.vue
+++ app/pages/data.vue
@@ -1,18 +1,35 @@
 <script setup>
-const response = await useFetch("/api/data");
-const tasks = ref(response["data"]);
+const data = (await useFetch("/api/data"))["data"];
 </script>
 
 <template>
   <h1>My Data</h1>
   <ul>
-    <li v-for="task in tasks">
-      <input v-for="key in task" :value="key" />
+    <li v-for="project in data">
+      <details>
+        <summary>
+          <input :value="project.name" />
+          <input :value="project.description" />
+        </summary>
+        <li v-for="task in project.tasks">
+          <input :value="task.name" />
+          <input :value="task.description" />
+          <input :value="task.status" />
+        </li>
+        <button>Add Task</button>
+      </details>
     </li>
+    <button>Add Project</button>
   </ul>
+  <button>Save</button>
 </template>
 
 <style>
+details li,
+details button {
+  margin-left: 30px;
+}
+
 ul {
   list-style-type: none;
   padding: 0px;
--- server/api/data.get.ts
+++ server/api/data.get.ts
@@ -4,9 +4,12 @@ export default defineCachedEventHandler((event) => {
   const { DB_URL } = useRuntimeConfig(event);
   const sql = neon(DB_URL);
   return sql`
-SELECT t.*, p.name AS project_name, p.description AS project_description
-FROM tasks t
-FULL OUTER JOIN projects_tasks pt ON t.id = pt.task_id
-FULL OUTER JOIN projects p ON pt.project_id = p.id
+SELECT p.id AS id, p.name AS name, p.description AS description, 
+       json_agg(json_build_object('id', t.id, 'name', t.name, 'description', t.description, 'status', t.status)) AS tasks
+FROM projects p
+LEFT JOIN projects_tasks pt ON p.id = pt.project_id
+LEFT JOIN tasks t ON pt.task_id = t.id
+GROUP BY p.id, p.name, p.description
+ORDER BY p.id
 `;
 });
