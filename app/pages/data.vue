2a390759084615d7a9d322ae57596f1f6117366d
jpatrick5402 <jpatrick5402@gmail.com>
2025-08-02 21:49:03 -0400

    data presented prettier

--- app/pages/data.vue
+++ app/pages/data.vue
@@ -1,11 +1,20 @@
 <script setup>
 const response = await useFetch("/api/data");
-const tasks = response["data"];
+const tasks = ref(response["data"]);
 </script>
 
 <template>
   <h1>My Data</h1>
   <ul>
-    <li v-for="task in tasks">{{ task }}</li>
+    <li v-for="task in tasks">
+      <input v-for="key in task" :value="key" />
+    </li>
   </ul>
 </template>
+
+<style>
+ul {
+  list-style-type: none;
+  padding: 0px;
+}
+</style>
