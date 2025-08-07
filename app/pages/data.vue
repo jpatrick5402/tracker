6817558875ce20690f29f4856935b58faf40d45c
jpatrick5402 <jpatrick5402@gmail.com>
2025-08-07 11:46:58 -0400

    style

--- app/pages/data.vue
+++ app/pages/data.vue
@@ -48,35 +48,39 @@ const rotation = ref(270);
 
 <template v-else>
   <h1>My Data</h1>
-  <ul>
+  <ul class="grid grid-cols-2">
     <li v-for="project in projects">
       <details class="flex m-3 bg-[#002FA7] border-white border-3 rounded-xl">
-        <summary class="flex p-6 m-3">
+        <summary class="flex p-6">
           <div>
+            <p>
+              {{
+                (project.tasks !== null ? project.tasks.length : 0) + " tasks"
+              }}
+            </p>
             <input :value="project.name" />
             <input :value="project.description" />
-            <Icon
-              name="material-symbols:delete-forever-outline-rounded"
-              @click="remove(project.id, 'project')"
-            />
+            <input :value="project.description" />
+            <button @click="remove(project.id, 'project')" class="bg-red-500">
+              <Icon name="material-symbols:delete-forever-outline-rounded" />
+            </button>
           </div>
         </summary>
-        <div class="m-3 bg-gray-800 p-4 rounded-xl border-3 border-black">
+        <div class="bg-gray-800 p-4 rounded-xl border-3 border-black">
           <li v-for="task in project.tasks">
             <input :value="task.name" />
             <input :value="task.description" />
             <input :value="task.status" />
-            <Icon
-              name="material-symbols:delete-forever-outline-rounded"
-              @click="remove(task.id, 'task')"
-            />
+            <button @click="remove(task.id, 'task')">
+              <Icon name="material-symbols:delete-forever-outline-rounded" />
+            </button>
           </li>
           <button @click="newTask(project.id)">Add Task</button>
         </div>
       </details>
     </li>
-    <button @click="newProject">Add Project</button>
   </ul>
+  <button @click="newProject">Add Project</button>
 </template>
 
 <style scoped>
@@ -84,13 +88,6 @@ input {
   border-radius: 10px;
   padding: 6px;
   background-color: #ff4f00;
-  margin: 3px;
-}
-
-button {
-  background-color: #002fa7;
-  border-radius: 10px;
-  padding: 6px;
   margin: 3px;
 }
 </style>
